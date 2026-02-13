b7db9f32c359f92542d97c6fc3a30a3fa9bae101
Sergio Frayle Perez <sfp932705@gmail.com>
2026-02-13 15:24:29 +0100

    Add trainer notes

--- src/tracker.py
+++ src/tracker.py
@@ -1,8 +1,10 @@
+import json
 import sys
 from datetime import datetime
 from typing import Any
 
 import torch
+from pydantic_settings import BaseSettings
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
@@ -10,16 +12,24 @@ from settings import TrackerSettings
 
 
 class ExperimentTracker:
-    def __init__(self, settings: TrackerSettings):
+    def __init__(self, settings: TrackerSettings, exp_settings: list[BaseSettings]):
         timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         self.run_dir = settings.runs_dir / f"{settings.run_name}_{timestamp}"
         self.log_dir = self.run_dir / "tensorboard"
         self.ckpt_dir = self.run_dir / "checkpoints"
         self.log_dir.mkdir(parents=True, exist_ok=True)
         self.ckpt_dir.mkdir(parents=True, exist_ok=True)
+        self.save_exp_settings(exp_settings)
         self.writer = SummaryWriter(log_dir=str(self.log_dir))
         self.progress_bar: tqdm | None = None
         self.global_step = 0
+
+    def save_exp_settings(self, settings: list[BaseSettings]):
+        sorted_settings = sorted(settings, key=lambda s: s.__class__.__name__)
+        settings_dict = {s.__class__.__name__: s.model_dump() for s in sorted_settings}
+        settings_path = self.run_dir / "settings.json"
+        with settings_path.open("w", encoding="utf-8") as f:
+            json.dump(settings_dict, f, indent=2)
 
     def save_artifact(self, name: str, state_dict: dict):
         save_path = self.ckpt_dir / name
