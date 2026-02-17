10a0dc13b3b61849c46cb9b2b356786e22d930fc
Sergio Frayle Perez <sfp932705@gmail.com>
2026-02-17 18:22:41 +0100

    Fix bug when updating special tokens

--- src/token_encoders/rust/bpe.py
+++ src/token_encoders/rust/bpe.py
@@ -3,6 +3,7 @@ from typing import Any
 
 import rust
 
+from token_encoders.base import BaseTokenizer
 from token_encoders.bpe import BPETokenizer
 from token_encoders.rust.base import RustBaseTokenizer
 
@@ -10,7 +11,7 @@ from token_encoders.rust.base import RustBaseTokenizer
 class RustBPETokenizer(RustBaseTokenizer, BPETokenizer):
     @property
     def backend_tokenizer(self) -> Any:
-        return rust.token_encoders.RustBPETokenizer  # type:ignore
+        return rust.token_encoders.RustBPETokenizer  # type: ignore
 
     @property
     def tokenizer_delimiter(self) -> str:
@@ -27,4 +28,5 @@ class RustBPETokenizer(RustBaseTokenizer, BPETokenizer):
     def load(self, directory: Path) -> None:
         BPETokenizer.load(self, directory)
         self._backend.set_state(self.vocab, self.merges)
-        self._update_special_tokens()
+        BaseTokenizer._update_special_tokens(self)
+        RustBaseTokenizer._update_special_tokens(self)
--- tests/token_encoders/rust/test_rust_wp.py
+++ tests/token_encoders/rust/test_rust_wp.py
@@ -1,4 +1,19 @@
+from pathlib import Path
+
+import pytest
+
 from token_encoders.rust.wp import RustWordPieceTokenizer
+
+
+@pytest.fixture
+def loaded_tokenizer(
+    trained_rust_word_piece_tokenizer: RustWordPieceTokenizer, tmp_path: Path
+) -> RustWordPieceTokenizer:
+    save_dir = tmp_path / "rust_wp_model"
+    trained_rust_word_piece_tokenizer.save(save_dir)
+    tokenizer = RustWordPieceTokenizer(trained_rust_word_piece_tokenizer.settings)
+    tokenizer.load(save_dir)
+    return tokenizer
 
 
 def test_rust_wp_training(trained_rust_word_piece_tokenizer: RustWordPieceTokenizer):
@@ -23,3 +38,21 @@ def test_rust_wp_encoding_decoding(
     assert "milk" in decoded
 
 
+def test_rust_wp_save(
+    trained_rust_word_piece_tokenizer: RustWordPieceTokenizer, tmp_path: Path
+):
+    save_dir = tmp_path / "rust_wp_model"
+    trained_rust_word_piece_tokenizer.save(save_dir)
+    assert (save_dir / "vocab.txt").exists()
+    assert (save_dir / "merges.txt").exists()
+
+
+@pytest.mark.parametrize(
+    "property_name",
+    ["pad_token_id", "mask_token_id", "cls_token_id", "sep_token_id", "unk_token_id"],
+)
+def test_rust_wp_load_special_tokens(
+    loaded_tokenizer: RustWordPieceTokenizer,
+    property_name: str,
+):
+    assert getattr(loaded_tokenizer, property_name) != -1
